1d6f95822ecd7ab37562cecd253adfdf112f1660
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-03-10 21:43:10 +0700

    +Add security layout
    -Create LoggedSecurityViewActivity
    -Update pass_req (add Log In button)

---
+++ Gallery/app/src/main/java/com/example/gallery/LoggedSecurityViewActivity.java
@@ -0,0 +1,67 @@
+package com.example.gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class LoggedSecurityViewActivity extends AppCompatActivity {
+    ImageButton btnAlbum;
+    ImageButton btnVideo;
+    ImageButton btnLoc;
+    ImageButton btnFav;
+    ImageButton btnSec;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.security_section);
+
+        btnAlbum = (ImageButton)findViewById(R.id.album_views);
+        btnAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoggedSecurityViewActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnVideo = (ImageButton)findViewById(R.id.video_views);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoggedSecurityViewActivity.this, VideoViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLoc = (ImageButton)findViewById(R.id.loc_views);
+        btnLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoggedSecurityViewActivity.this, LocationViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFav = (ImageButton)findViewById(R.id.fav_views);
+        btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoggedSecurityViewActivity.this, FavoriteViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSec = (ImageButton)findViewById(R.id.sec_views);
+        btnSec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoggedSecurityViewActivity.this, SecurityViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- Gallery/app/src/main/java/com/example/gallery/SecurityViewActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/SecurityViewActivity.java
@@ -5,63 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class SecurityViewActivity extends AppCompatActivity {
-    ImageButton btnAlbum;
-    ImageButton btnVideo;
-    ImageButton btnLoc;
-    ImageButton btnFav;
-    ImageButton btnSec;
+    Button btnLogIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.security_section);
+        setContentView(R.layout.pass_req);
 
-        btnAlbum = (ImageButton)findViewById(R.id.album_views);
-        btnAlbum.setOnClickListener(new View.OnClickListener() {
+        btnLogIn = (Button)findViewById(R.id.logInButton);
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecurityViewActivity.this, MainActivity.class);
+                Intent intent = new Intent(SecurityViewActivity.this, LoggedSecurityViewActivity.class);
                 startActivity(intent);
             }
         });
-
-        btnVideo = (ImageButton)findViewById(R.id.video_views);
-        btnVideo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecurityViewActivity.this, VideoViewActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnLoc = (ImageButton)findViewById(R.id.loc_views);
-        btnLoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecurityViewActivity.this, LocationViewActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnFav = (ImageButton)findViewById(R.id.fav_views);
-        btnFav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecurityViewActivity.this, FavoriteViewActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnSec = (ImageButton)findViewById(R.id.sec_views);
-        btnSec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecurityViewActivity.this, SecurityViewActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
