9c3a6c0e1eb80371389513d66dfd81c28ffe0ffa
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-04-21 21:57:34 +0700

    +Delete image
    -Allow write permission in manifest
    -Add temp pop up menu in full screen image view
    -Add temp menu icon
    -Add temp Image button in layout
    -Add new menu

--- Gallery/app/src/main/java/com/example/gallery/FullScreenActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/FullScreenActivity.java
@@ -2,23 +2,84 @@ package com.example.gallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 
 public class FullScreenActivity extends AppCompatActivity {
+    ImageButton btnMore;
+    String path;
     public static Bitmap rotateImage(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
                 matrix, true);
+    }
+
+    private void showMenu(){
+        PopupMenu popupMenu = new PopupMenu(this, btnMore);
+        popupMenu.getMenuInflater().inflate(R.menu.setting_img_view, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete:
+                        deleteImage(path);
+                        Intent intent = new Intent(FullScreenActivity.this, MainActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                    case R.id.details:
+                        Toast.makeText(getApplicationContext(), "Detail clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+    }
+
+    private void deleteImage(String path) {
+        File file = new File(path);
+
+        String[] projection = {MediaStore.Images.Media._ID};
+
+        // Match on the file path
+        String selection = MediaStore.Images.Media.DATA + " = ?";
+        String[] selectionArgs = new String[]{file.getAbsolutePath()};
+
+        // Query for the ID of the media matching the file path
+        Uri queryUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        ContentResolver contentResolver = getContentResolver();
+        Cursor c = contentResolver.query(queryUri, projection, selection, selectionArgs, null);
+        if (c.moveToFirst()) {
+            // We found the ID. Deleting the item via the content provider will also remove the file
+            long id = c.getLong(c.getColumnIndexOrThrow(MediaStore.Images.Media._ID));
+            Uri deleteUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            contentResolver.delete(deleteUri, null, null);
+            Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Something wrong", Toast.LENGTH_SHORT).show();
+        }
+        c.close();
     }
 
     @Override
@@ -27,8 +88,20 @@ public class FullScreenActivity extends AppCompatActivity {
         setContentView(R.layout.image_screen);
 
         Intent i = getIntent();
-        String path = i.getExtras().getString("path");
+        path = i.getExtras().getString("path");
         File imgFile = new  File(path);
+
+        btnMore = findViewById(R.id.btnMore);
+        btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenu();
+            }
+        });
+
+
+
+
 
         if(imgFile.exists()){
 
