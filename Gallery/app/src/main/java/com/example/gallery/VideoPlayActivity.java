6d9d907165220e57ee0a9bdbb2fea889e37747cb
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-05-09 00:47:43 +0700

    +Add delete video function

--- Gallery/app/src/main/java/com/example/gallery/VideoPlayActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/VideoPlayActivity.java
@@ -3,10 +3,15 @@ package com.example.gallery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.WallpaperManager;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
@@ -73,15 +78,47 @@ public class VideoPlayActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.delete:
+                    case R.id.delete_vid:
+                        deleteVideo(str_video);
+                        Intent intent = new Intent(VideoPlayActivity.this, VideoViewActivity.class);
+                        finish();
+                        startActivity(intent);
                         break;
-                    case R.id.details:
-
+                    case R.id.details_vid:
+                        vv_video.pause();
+                        Intent intent2 = new Intent(VideoPlayActivity.this, VideoDetailsScreen.class);
+                        finish();
+                        intent2.putExtra("path", str_video);
+                        startActivity(intent2);
                         break;
                 }
                 return true;
             }
         });
         popupMenu.show();
+    }
+    private void deleteVideo(String path) {
+        File file = new File(path);
+
+        String[] projection = {MediaStore.Video.Media._ID};
+
+        // Match on the file path
+        String selection = MediaStore.Video.Media.DATA + " = ?";
+        String[] selectionArgs = new String[]{file.getAbsolutePath()};
+
+        // Query for the ID of the media matching the file path
+        Uri queryUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        ContentResolver contentResolver = getContentResolver();
+        Cursor c = contentResolver.query(queryUri, projection, selection, selectionArgs, null);
+        if (c.moveToFirst()) {
+            // We found the ID. Deleting the item via the content provider will also remove the file
+            long id = c.getLong(c.getColumnIndexOrThrow(MediaStore.Video.Media._ID));
+            Uri deleteUri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
+            contentResolver.delete(deleteUri, null, null);
+            Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Something wrong", Toast.LENGTH_SHORT).show();
+        }
+        c.close();
     }
 }
