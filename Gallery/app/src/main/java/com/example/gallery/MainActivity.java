bbe9b2142438c970eb4a4fa509c09b0023ced5d7
TUANANHLC <tuananhlc@nvpcloud.com>
2021-05-14 01:37:17 +0700

    Update MainActivity.java

--- Gallery/app/src/main/java/com/example/gallery/MainActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
@@ -102,7 +103,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+<<<<<<< HEAD
         
+=======
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA
+                    }, MY_PERMISSION_CODE);
+        } else {
+            try {
+                loadImages(1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+>>>>>>> 7b29e62fee1b2bee6e6e42682766b1ef88d47399
 //        if(ContextCompat.checkSelfPermission(MainActivity.this,
 //                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
 //            ActivityCompat.requestPermissions(MainActivity.this,
@@ -114,6 +134,16 @@ public class MainActivity extends AppCompatActivity {
 //                    Manifest.permission.CAMERA
 //            }, 100);
 //        }
+
+        btnVideo = (ImageButton)findViewById(R.id.videos_view);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VideoViewActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
 
         btnPhoto = (ImageButton)findViewById(R.id.photos_view);
         btnPhoto.setOnClickListener(new View.OnClickListener() {
@@ -182,7 +212,20 @@ public class MainActivity extends AppCompatActivity {
                 finish();
                 startActivity(intent_people);
                 break;
-
+            case R.id.change_grid_layout:
+                try {
+                    loadImages(1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.change_linear_layout:
+                try {
+                    loadImages(2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -195,9 +238,14 @@ public class MainActivity extends AppCompatActivity {
         getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
     }
 
-    private void loadImages() throws IOException {
+    private void loadImages(int opt) throws IOException {
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        if (opt == 1) {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        }
+        else if (opt == 2){
+            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        }
         images = ImagesGallery.listOfImages(this);
         galleryAdapter = new GalleryAdapter(this, images, new GalleryAdapter.PhotoListener() {
             @Override
@@ -218,7 +266,7 @@ public class MainActivity extends AppCompatActivity {
             case MY_PERMISSION_CODE:{
                 Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
                 try {
-                    loadImages();
+                    loadImages(1);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
